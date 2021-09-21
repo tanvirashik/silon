a9bdde1d06c1c9c96e6b8d2d8470036dc8ffa160
Tanvir Ashik <86204933+tanvirashik@users.noreply.github.com>
2021-09-22 02:33:07 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,103 @@
+$(document).ready(function(){
+
+  bbtn = new WOW(
+    {
+    boxClass:     'bbtn',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  bbtn.init();
+  // e
+  ab = new WOW(
+    {
+    boxClass:     'ab',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  ab.init();
+  // e
+  // e
+  blog_content = new WOW(
+    {
+    boxClass:     'blog_content',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  blog_content.init();
+      // e
+  insta = new WOW(
+    {
+    boxClass:     'insta',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  insta.init();
+  // e
+  feat = new WOW(
+    {
+    boxClass:     'feat',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  feat.init();
+  // e
+  title = new WOW(
+    {
+    boxClass:     'title',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  title.init();
+  // e
+  abtitle = new WOW(
+    {
+    boxClass:     'abtitle',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  abtitle.init();
+  // e
+  banner2title = new WOW(
+    {
+    boxClass:     'banner2title',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  banner2title.init();
+  // e
+  pjq = new WOW(
+    {
+    boxClass:     'pjq',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  pjq.init();
+
+})
